bbd5108c072f820908b72acba22c33dc3da01a72
Piyush Yadav <koshijeager@gmail.com>
2026-01-06 22:46:35 +0530

    3.5 Working on posts

---
+++ src/app/Posts/layout.tsx
@@ -0,0 +1,18 @@
+import { AppSidebar } from "@/components/My Components/Dashboard/AppHeader"
+import Dashboard from "@/components/My Components/Dashboard/Dashboar"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <SidebarProvider>
+            <AppSidebar />
+            <main>
+                <SidebarTrigger />
+                <Dashboard />
+
+                {children}
+            </main>
+        </SidebarProvider>
+    )
+}
