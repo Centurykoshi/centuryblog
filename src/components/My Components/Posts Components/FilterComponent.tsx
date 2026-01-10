fd5936e2b4414d4f3b519b26549a27f3a3203dca
Piyush Yadav <koshijeager@gmail.com>
2026-01-10 22:13:45 +0530

    4.2 Done with posts filter and also with link

--- src/components/My Components/Posts Components/FilterComponent.tsx
+++ src/components/My Components/Posts Components/FilterComponent.tsx
@@ -20,22 +20,34 @@ export default function FilterPostComponent({ value, onChange }: Props) {
   ];
 
   return (
-    <div className="flex justify-center">
-      <motion.div className="flex gap-2 sm:gap-6">
-        {states.map((state) => (
-          <Button
-            key={state}
-            variant="ghost"
-            className={cn(
-              "border-none font-medium text-base relative z-10 transition-colors duration-200",
-              value === state && "text-primary-foreground"
-            )}
-            onClick={() => onChange(state)}
-          >
-            {state}
-          </Button>
-        ))}
-      </motion.div>
-    </div>
+    <>
+      <div className="flex justify-center">
+        <motion.div className="flex gap-2 sm:gap-6">
+          {states.map((state, index) => (
+            <motion.div key={state}
+            className="relative border-none" 
+            initial={{opacity : 0, scale : 0.8}}
+            animate={{opacity : 1, scale : 1}}
+            transition={{delay : index * 0.2, duration : 0.3}}
+            whileHover={{scale : 0.9}}
+            whileTap={{scale : 0.95}}>
+              <Button
+               
+                variant="ghost"
+                className={cn(
+                  "border-none font-medium text-base relative z-10 transition-colors duration-200 text-secondary-foreground/70",
+                  value === state && "text-secondary-foreground"
+                )}
+                onClick={() => onChange(state)}
+              >
+                {state}
+
+                {state === value && (<motion.div layoutId="activeButton" className="absolute inset-0 bg-primary/60 rounded-lg -z-10 blur"/>)}
+              </Button>
+            </motion.div>
+          ))}
+        </motion.div>
+      </div>
+    </>
   );
 }
