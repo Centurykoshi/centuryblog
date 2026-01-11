06450e9b2c15ff3181b521437207d412fcf32709
Piyush Yadav <koshijeager@gmail.com>
2026-01-11 23:41:24 +0530

    5.2 Fixed Heading and image in the editor page

---
+++ src/components/tiptap-templates/simple/TagsSeletion.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+type Tags = "All | Tech | LifeStyle | Games | Travel "
+
+type Props = {
+    value: Tags;
+    onChange: (tag: Tags) => void;
+};
+
+
+export default function tagsSelection() {
+
+
+
+
+
+
+}
