312d4cf1572438aff7486dd0e0b4d2d89c2466a4
Piyush Yadav <koshijeager@gmail.com>
2026-02-03 22:49:06 +0530

    er

--- src/app/NewPost/layout.tsx
+++ src/app/NewPost/layout.tsx
@@ -13,6 +13,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                 <SidebarTrigger />
                 <SidebarDashboard />
 
+
+                
+
                 {children}
             </main>
         </SidebarProvider>
